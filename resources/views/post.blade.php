84eca64ee3b4b17db4dd034dbe01103afa8d1bc1
Eric Russell <Erictim.Russell@live.com>
2023-07-26 08:59:17 -0600

    hard code layouts

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1>Blog Post</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis modi id aspernatur accusamus iste. Fugiat debitis mollitia repellendus atque tempore at ullam dolorum aliquid veritatis quasi. Tenetur excepturi libero voluptates.
+        </p>
+
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <a href="/post"><h1>Blog Post</h1></a>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis modi id aspernatur accusamus iste. Fugiat debitis mollitia repellendus atque tempore at ullam dolorum aliquid veritatis quasi. Tenetur excepturi libero voluptates.
+        </p>
+
+    </article>
+
+    <article>
+        <a href="/post"><h1>Blog Post</h1></a>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis modi id aspernatur accusamus iste. Fugiat debitis mollitia repellendus atque tempore at ullam dolorum aliquid veritatis quasi. Tenetur excepturi libero voluptates.
+        </p>
+        
+    </article>
+
+    <article>
+        <a href="/post"><h1>Blog Post</h1></a>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis modi id aspernatur accusamus iste. Fugiat debitis mollitia repellendus atque tempore at ullam dolorum aliquid veritatis quasi. Tenetur excepturi libero voluptates.
+        </p>
+        
+    </article>
+</body>
