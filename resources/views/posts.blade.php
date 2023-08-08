968b84e5ed55202e0b0bbd4e88ca791c3bdd2a1a
Eric Russell <Erictim.Russell@live.com>
2023-08-08 09:30:29 -0600

    php practice

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,6 +4,29 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
+
+    <?php
+
+    $name = "Dark Matter";
+    $read = true;
+
+
+    if ($read) {
+        $message = "You have read $name";
+    } else {
+        $message = "You have not read $name";
+    }
+    ?>
+
+    <h1>
+        <!-- Long hand way to display html -->
+        <!-- <?php echo $message; ?> -->
+
+        <!-- Short hand way to display html -->
+        <?= $message ?>
+    </h1>
+
+
     <!-- iterate over each post -->
     <?php foreach ($posts as $post) : ?>
         <article>
